70694407b226ddb1717bba3e4ed793968164ba97
Francelino Marcilio <fmarcilio1989@gmail.com>
2019-10-29 22:40:01 -0300

    tela de inicio quase pronto

---
+++ app/src/main/java/com/example/book_tads/activity/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.book_tads.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.book_tads.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonEnviar.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+
+    }
+}
